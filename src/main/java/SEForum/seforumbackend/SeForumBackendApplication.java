1a51bce176a28417bee1c7a88bdb38883c72c5cc
domi-schuhmi <dschuhmann96@gmail.com>
2022-10-27 09:31:14 +0200

    Initial commit

---
+++ src/main/java/SEForum/seforumbackend/SeForumBackendApplication.java
@@ -0,0 +1,13 @@
+package SEForum.seforumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeForumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeForumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SEForum/seforumbackend/SeForumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SEForum.seforumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeForumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
